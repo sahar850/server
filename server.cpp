b124704fbd97bc7ef902b7d9430973d8f9c79122
sahar850 <sahar850@gmail.com>
2017-12-03 16:23:40 +0200

    new project - server

---
+++ server.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sahar on 03/12/17.
+//
+
+#include "server.h"
---
+++ server.h
@@ -0,0 +1,14 @@
+//
+// Created by sahar on 03/12/17.
+//
+
+#ifndef SERVER_SERVER_H
+#define SERVER_SERVER_H
+
+
+class server {
+
+};
+
+
+#endif //SERVER_SERVER_H
